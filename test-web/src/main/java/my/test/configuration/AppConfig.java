e98b4fce653f42735eb33d4618e56cbb851cdab9
dragan susak <dragan.susak@live.com>
2016-04-17 15:59:19 +0200

    Formatting

--- test-web/src/main/java/my/test/configuration/AppConfig.java
+++ test-web/src/main/java/my/test/configuration/AppConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @ComponentScan(basePackages = {"my.test"})
 public class AppConfig extends WebMvcConfigurerAdapter{
+
 //    @Override
 //    public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
