72bfef2dfeff5cd679d1be972e826dd55cff3c48
Dragan Susak <Dragan Susak@zerosones.local>
2016-06-17 12:37:49 +0200

    Added reactjs table component

--- test-web/src/main/java/my/test/controller/IndexController.java
+++ test-web/src/main/java/my/test/controller/IndexController.java
@@ -19,4 +19,9 @@ public class IndexController {
     public  String getUserDetails(){
         return "userDetails.html";
     }
+
+    @RequestMapping(value="/", method = RequestMethod.GET)
+    public  String getIndex(){
+        return "index.html";
+    }
 }
