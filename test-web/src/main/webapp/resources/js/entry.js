900d7d4da71479d8acf8c36bce1b426ba221a628
Dragan Susak <dragan.susak@live.com>
2016-06-22 14:50:56 +0200

    Include css in webpack

--- test-web/src/main/webapp/resources/js/entry.js
+++ test-web/src/main/webapp/resources/js/entry.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./hello";
+import './style';
 
-ReactDOM.render(<Hello name="aae"/>, document.getElementById("blabla"));
+ReactDOM.render(<Hello name="eeee"/>, document.getElementById("blabla"));
